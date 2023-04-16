29a0dc9b60c229eb91c57fe5c741d128bbcdce1f
Phuzzy112 <Phuzzy112@users.noreply.github.com>
2023-04-16 20:27:29 +1000

    Update mkfw.sh

--- mkfw.sh
+++ mkfw.sh
@@ -22,7 +22,7 @@ cd buildroot/output/images/
 ./genSWU.sh
 cd -
 
-cp -rf ./buildroot/output/images/turing_pi2_ce-${version}.swu ./build/${date}/turing_pi2_ce-${version}.swu
+cp -rf ./buildroot/output/images/turing_pi_2_ce-${version}.swu ./build/${date}/turing_pi2_ce-${version}.swu
 
 echo "build turing pi firmware over"
 
